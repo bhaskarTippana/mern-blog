fa242e1b2921b260824249b2dbc3119ede1275ec
bhaskarTippana <bhaskarbunny1371@gmail.com>
2023-11-27 23:14:59 +0530

    hbjh

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <h1 className='bg-green-500 p-3'>hello rdrctfctf</h1>
+    <h1 className='bg-green-500 p-3'>hello rdrctfctf jhgjgjugu</h1>
   )
 }
 
